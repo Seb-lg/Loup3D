334e04258febe577e637a21436465f6030ff97c4
zodiac <jean-baptiste.dupuy@epitech.eu>
2022-03-02 20:27:32 +0100

    moved HandleInput to inputs.c

--- src/inputs.c
+++ src/inputs.c
@@ -3,6 +3,49 @@
 #include "../inc/conf.h"
 #include "../inc/inputs.h"
 
+void HandleInput(GLFWwindow *window) {
+  if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
+    player.posX += cos(player.angle * (M_PI / 180.0)) / 10;
+    player.posY += sin(player.angle * (M_PI / 180.0)) / 10;
+    player.posX = player.posX < 0         ? 0
+                  : player.posX > MapSize ? MapSize
+                                          : player.posX;
+    player.posY = player.posY < 0         ? 0
+                  : player.posY > MapSize ? MapSize
+                                          : player.posY;
+  }
+  if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
+    player.posX -= cos(player.angle * (M_PI / 180.0)) / 10;
+    player.posY -= sin(player.angle * (M_PI / 180.0)) / 10;
+    player.posX = player.posX < 0         ? 0
+                  : player.posX > MapSize ? MapSize
+                                          : player.posX;
+    player.posY = player.posY < 0         ? 0
+                  : player.posY > MapSize ? MapSize
+                                          : player.posY;
+  }
+  if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
+    player.posX -= cos((player.angle + 90.) * (M_PI / 180.0)) / 10;
+    player.posY -= sin((player.angle + 90.) * (M_PI / 180.0)) / 10;
+    player.posX = player.posX < 0         ? 0
+                  : player.posX > MapSize ? MapSize
+                                          : player.posX;
+    player.posY = player.posY < 0         ? 0
+                  : player.posY > MapSize ? MapSize
+                                          : player.posY;
+  }
+  if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
+    player.posX -= cos((player.angle - 90.) * (M_PI / 180.0)) / 10;
+    player.posY -= sin((player.angle - 90.) * (M_PI / 180.0)) / 10;
+    player.posX = player.posX < 0         ? 0
+                  : player.posX > MapSize ? MapSize
+                                          : player.posX;
+    player.posY = player.posY < 0         ? 0
+                  : player.posY > MapSize ? MapSize
+                                          : player.posY;
+  }
+}
+
 void key_callback(GLFWwindow *window, int key, int scancode, int action,
                   int mods) {
   if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
